527fafe2b679be7c5a6c9d88510931d3dc422adf
TankNut <m.vlasblom@live.nl>
2019-11-04 16:47:19 +0100

    Wearing a combine suit makes you friendly to combine NPC's

--- lua/entities/suit_combine.lua
+++ lua/entities/suit_combine.lua
@@ -25,6 +25,26 @@ ENT.SuitData = {
 	}
 }
 
+if SERVER then
+	ENT.SuitData.NPCRelationships = {
+		[CLASS_COMBINE] = D_LI,
+		[CLASS_COMBINE_GUNSHIP] = D_LI,
+		[CLASS_MANHACK] = D_LI,
+		[CLASS_METROPOLICE] = D_LI,
+		[CLASS_MILITARY] = D_LI,
+		[CLASS_SCANNER] = D_LI,
+		[CLASS_STALKER] = D_LI,
+		[CLASS_PROTOSNIPER] = D_LI,
+		[CLASS_COMBINE_HUNTER] = D_LI,
+		[CLASS_PLAYER_ALLY] = D_HT,
+		[CLASS_PLAYER_ALLY_VITAL] = D_HT,
+		[CLASS_CITIZEN_PASSIVE] = D_HT,
+		[CLASS_CITIZEN_REBEL] = D_HT,
+		[CLASS_VORTIGAUNT] = D_HT,
+		[CLASS_HACKED_ROLLERMINE] = D_HT
+	}
+end
+
 if CLIENT then
 	function ENT:RenderScreenspaceEffects()
 		local ply = LocalPlayer()
