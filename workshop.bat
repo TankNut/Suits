e932718019b86b83f7cafc6049717bf5a65a5b75
TankNut <m.vlasblom@live.nl>
2019-11-04 15:35:58 +0100

    Set up addon.json and workshop upload script

---
+++ workshop.bat
@@ -0,0 +1,11 @@
+set WORKSHOP_ID = 1905984764
+
+cd /d "%~dp0"
+cd ../../../bin/
+
+gmad create -folder "%~dp0\" -out "%~dp0\..\__TEMP.gma"
+gmpublish update -addon "%~dp0\..\__TEMP.gma" -id "%WORKSHOP_ID%"
+
+del %~dp0\..\__TEMP.gma
+
+pause
